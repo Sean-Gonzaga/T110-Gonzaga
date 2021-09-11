afb2b343bcf9aeb2cfb99a858e5d93ba51f89d1c
Sean-Gonzaga <90443316+Sean-Gonzaga@users.noreply.github.com>
2021-09-11 10:53:59 +0800

    Add files via upload

---
+++ T110_MiAct1.java
@@ -0,0 +1,10 @@
+public class T110_MiAct1 {
+    public static void main(String[] args) {
+
+        System.out.println(992 + 8);
+        System.out.println(60 % 2);
+        System.out.println(22 - 22);
+        System.out.println(21 % 2);
+        System.out.println(20 / 5);
+    }
+}
