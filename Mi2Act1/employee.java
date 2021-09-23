aac7eb67d8fcffb89f2ff44f708b13700decde37
Sean-Gonzaga <90443316+Sean-Gonzaga@users.noreply.github.com>
2021-09-23 16:51:37 +0800

    Add files via upload

---
+++ Mi2Act1/employee.java
@@ -0,0 +1,17 @@
+
+//defining an employee class
+public class employee {
+
+    //defining fields
+    String name;//field or data member or instance variable 
+    int id;
+    double salary;
+
+    //constructor
+    employee (String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;   
+
+    }
+}
