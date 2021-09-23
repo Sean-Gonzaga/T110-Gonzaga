b1817458b7ca6997a0b6ed73078b0079973e26ce
Sean-Gonzaga <90443316+Sean-Gonzaga@users.noreply.github.com>
2021-09-23 10:21:01 +0800

    Add files via upload

---
+++ MiAct2.java
@@ -0,0 +1,20 @@
+public class MiAct2 {
+    public static void main(String[] args) {
+        
+
+        //A spinning tire makes 3000 revolutions per minute. 
+        //Calculate How many degrees it rotate in one second.
+        
+        //declaration of variables
+        int revs = 3000, deg = 360; 
+        int dps, rps;
+
+        //calculations
+        rps = revs / 60;
+        dps = rps * deg;
+
+        
+        System.out.printf(dps + " degrees per second");//printing the degree per second
+    }
+    
+}
